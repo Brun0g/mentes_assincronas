a9e8fd7806358d68eb9c870a551a02f31f2720eb
Bruno Gabriel <brun0_g@hotmail.com>
2023-11-24 16:01:49 -0300

    chore: dump.sql

---
+++ src/model/dump.sql
@@ -0,0 +1,11 @@
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nome TEXT,
+    email TEXT UNIQUE,
+    senha TEXT
+);
+
+CREATE TABLE categorias (
+    id SERIAL PRIMARY KEY,
+    descricao text
+);
