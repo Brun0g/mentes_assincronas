179867923e00f2aab63b7549338965a2d24268ed
Bruno Vallini <87041579+Brun0g@users.noreply.github.com>
2023-11-27 16:09:48 -0300

    feat: conflicts routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,43 @@ const express = require("express");
 
 const { registrationValidation } = require("./middleware/registration_validation");
 const { userRegistration } = require("./controller/user_registration");
+
 const insertCategories = require("./controller/insert_categories");
 const listCategory = require("./controller/list_categories");
+
+const { registerUser, loginUser, userProfile, updateUser } = require('./controller/userLogin');
+const authenticateLogin = require('./middleware/authLogin');
+
 const routes = express();
 
 routes.post("/usuario", registrationValidation, userRegistration);
 
 routes.post("/insertCategory", insertCategories);
 routes.get("/listCategory", listCategory);
+
+routes.post('/usuario', registerUser);
+routes.post('/login', loginUser);
+routes.use(authenticateLogin);
+routes.get('/usuario', userProfile);
+routes.put('/usuario', updateUser);
+
 module.exports = routes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
