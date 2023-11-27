d147aa1bf1014133b82cc11d17eabb5e8682ade9
Bruno Gabriel <brun0_g@hotmail.com>
2023-11-27 14:18:33 -0300

    feat:categories, user_registration

---
+++ src/controller/insert_categories.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const { pool } = require("../model/model.js");
+
+const insertCategories = async (req, res) => {
+  const { descricao } = req.body;
+
+  if (!descricao) {
+    return res.status(400).json({ message: "Description must be filled." });
+  }
+
+  try {
+    await pool.query("INSERT INTO categorias (descricao) VALUES ($1) returning id, descricao", [descricao]);
+    return res.status(201).send();
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = insertCategories;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const { registrationValidation } = require('./middleware/registration_validation');
-const { userRegistration } = require('./controller/user_registration')
+const express = require("express");
 
+const { registrationValidation } = require("./middleware/registration_validation");
+const { userRegistration } = require("./controller/user_registration");
+const insertCategories = require("./controller/insert_categories");
+const listCategory = require("./controller/list_categories");
 const routes = express();
 
-routes.post('/usuario', registrationValidation, userRegistration);
+routes.post("/usuario", registrationValidation, userRegistration);
 
+routes.post("/insertCategory", insertCategories);
+routes.get("/listCategory", listCategory);
 module.exports = routes;
