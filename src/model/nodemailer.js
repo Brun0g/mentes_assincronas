5a6f30232bcc3f3edf73e32e99077b4c8cd8a9a7
Bruno Gabriel <brun0_g@hotmail.com>
2023-12-12 20:27:09 -0300

    fix

--- src/model/nodemailer.js
+++ src/model/nodemailer.js
@@ -1,18 +1,14 @@
 const nodemailer = require("nodemailer");
 
-async function createTransporter() {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-    secure: true,
-    tls: { rejectUnauthorized: true },
-  });
+const transportador = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+  secure: true,
+  tls: { rejectUnauthorized: false },
+});
 
-  return transporter;
-}
-
-module.exports = createTransporter;
+module.exports = transportador;
